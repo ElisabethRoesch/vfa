3b780899401b0bd712d997cd7107d9c02114e8a3
LislPisl <eroesch@student.unimelb.edu.au>
2019-08-26 14:12:55 +1000

    test data collection

--- src/vfa.jl
+++ src/vfa.jl
@@ -1,5 +1,5 @@
 module vfa #Plots, Optim, Dates, DiffEqParamEstim, Flux, DiffEqFlux, Statistics,
-    export vfa_1, spindle_C
+    export vfa_1, get_spindle_data
     include("vfa_methods.jl")
     include("../data/spindle_C.jl")
 end
--- src_test/test_vfa.jl
+++ src_test/test_vfa.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH, "/Users/eroesch/github")
 using vfa
 
 vfa_1()
-
+spindle_C =get_spindle_data()
 a = CCNB1[:,2]
 b = pMPS1[:,2]
 c = pENSA[:,2]
