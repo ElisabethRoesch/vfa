dd1c10477d6589e29a3b36de304caf4b57237bdd
LislPisl <eroesch@student.unimelb.edu.au>
2019-08-26 16:36:36 +1000

    grid , solve, add.

--- src/vfa.jl
+++ src/vfa.jl
@@ -1,6 +1,6 @@
 module vfa #Plots, Optim, Dates, DiffEqParamEstim, Flux, DiffEqFlux, Statistics,
-    using Plots
-    export get_spindle_C_data, plot_path, get_coord_list
+    using Plots, Flux
+    export get_spindle_C_data, plot_path, get_coord_list, get_grad_and_end_point_list
     include("vfa_methods.jl")
     include("../data/spindle_C.jl")
     include("../visualize/pre_ana.jl")
--- src_test/spindle_C/test_vfa_spindle_C.jl
+++ src_test/spindle_C/test_vfa_spindle_C.jl
@@ -7,13 +7,26 @@ plt = plot_path(CCNB1, pMPS1, pENSA)
 grid_ranges = [range(0, step=0.1, stop=1), range(0, step=0.1, stop=1), range(0, step=0.1, stop=1)]
 coord_list = get_coord_list(grid_ranges)
 ##################
-@load "spindle_C.bson" dudt
+@load "src_test/spindle_C/spindle_C.bson" dudt
+species = ["CCNB1", "pMPS1", "pENSA"]
+tspan = (0.,1.)
+a = CCNB1[:,2]
+b = pMPS1[:,2]
+c = pENSA[:,2]
+ode_data = transpose(hcat( a,b,c))
+u0 = ode_data[:,1]
+if(CCNB1[1]>0)
+    t = CCNB1[:,1].-(CCNB1[1])
+else
+    t = CCNB1[:,1].+abs(CCNB1[1])
+end
+t = t./t[end]
+n_ode = x->neural_ode(dudt, x, tspan, Rosenbrock23(autodiff=false), saveat=t, reltol=1e-7, abstol=1e-9)
 pred = n_ode(u0)
-scatter(t, ode_data[1,:], label = string("Observation: ", species[1]), grid = "off", ylim=[0,1.3])
+plt = scatter(t, ode_data[1,:], label = string("Observation: ", species[1]), grid = "off", ylim=[0,1.3])
 scatter!(t, ode_data[2,:], label = string("Observation: ", species[2]))
 scatter!(t, ode_data[3,:], label = string("Observation: ", species[3]))
 plot!(t, Flux.data(pred[1,:]), label = string("Prediction: ", species[1]))
 plot!(t, Flux.data(pred[2,:]), label = string("Prediction: ", species[2]))
 plot!(t, Flux.data(pred[3,:]), label = string("Prediction: ", species[3]))
 grad_list, end_coord_list = get_grad_and_end_point_list(coord_list, dudt)
-##################
