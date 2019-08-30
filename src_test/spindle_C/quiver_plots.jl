aae4802ae430a541fce265aefc2a69eefe3014b6
LislPisl <eroesch@student.unimelb.edu.au>
2019-08-31 09:12:24 +1000

    quiver plot of Plots.jl in 3D for neural ODE.

--- src_test/spindle_C/quiver_plots.jl
+++ src_test/spindle_C/quiver_plots.jl
@@ -4,5 +4,6 @@ y=[1,2,3]
 z=[1,2,3]
 u=[1,2,3]
 v=[1,2,3]
-w=[1,2,3]
+w=[1,2,3
+# here I am using Plots quiver plot
 plt = quiver(coord_list[1,:], coord_list[2,:], coord_list[3,:], quiver=(grad_list[1,:],grad_list[2,:], grad_list[3,:]), projection="3d")
