37f5b1d17543d1881461d54e57d4883da0074a2b
LislPisl <eroesch@student.unimelb.edu.au>
2019-12-01 10:22:59 +1100

    ks loss intro

--- src_test/bifurcations_1d/pitchfork_bifur/ks_fix_alpha_2.jl
+++ src_test/bifurcations_1d/pitchfork_bifur/ks_fix_alpha_2.jl
@@ -1,7 +1,7 @@
 
 push!(LOAD_PATH, "/Users/eroesch/github")
-using vfa, GR
-using  DifferentialEquations
+using vfa
+using  DifferentialEquations, StatsBase
 using Plots, Optim, Dates, DiffEqParamEstim, Flux, DiffEqFlux, Statistics, LinearAlgebra, OrdinaryDiffEq
 using BSON: @save, @load
 datasize = 35
@@ -35,7 +35,6 @@ plot!(Array(range(1,stop = datasize)),ode_data[5])
 
 
 
-
 dudt = Chain(Dense(1,15,tanh),
        Dense(15,15,tanh),
        Dense(15,1))
@@ -43,22 +42,39 @@ ps = Flux.params(dudt)
 n_ode = x->neural_ode(dudt, x, tspan, Tsit5(), saveat=t, reltol=1e-7, abstol=1e-9)
 n_epochs = 200
 data1 = Iterators.repeated((), n_epochs)
-#opt1 = ADAM(0.0001)
 opt1 = Descent(0.0005)
-#L2_loss_fct() = sum(abs2,ode_data .- n_ode(u0))+sum(abs2,ode_data2 .- n_ode(u02))
-function L2_loss_fct()
+Array{Float64,1}(1000)
+function kolmogorov_smirnov_distance(data1,data2)
+            ecdf_func_1 = StatsBase.ecdf(data1)
+            ecdf_func_2 = StatsBase.ecdf(data2)
+            max = maximum([data1;data2])
+            intervals = max/999
+            ecdf_vals_1 = Array{Float64,1}(undef, 1000)
+            for i in 1:1000
+                        ecdf_vals_1[i]=ecdf_func_1(intervals*(i-1))
+            end
+            ecdf_vals_2 = Array{Float64,1}(undef, 1000)
+            for i in 1:1000
+                        ecdf_vals_2[i]=ecdf_func_2(intervals*(i-1))
+            end
+            dist = maximum(abs.(ecdf_vals_1-ecdf_vals_2))
+            return dist
+end
+
+
+function loss_fct()
     sum = 0.0
     counter = 0
     for i in train_u0s
         counter = counter+1
-        s = LinearAlgebra.norm(ode_data[counter[1]].-n_ode([i]))
+        s = kolmogorov_smirnov_distance(ode_data[counter[1]], reshape(n_ode([i]),length(n_ode([i]))))
         sum=sum+s
     end
     return sum
 end
 
 cb1 = function ()
-    println(Tracker.data(L2_loss_fct()))
+    println(Tracker.data(loss_fct()))
 end
 test_u0s = [-3.,-2.5,-2.,-1.5,-1.,-0.5,0.,0.5,1.,1.5,2.,2.5,-3.]
 preds = []
@@ -82,7 +98,7 @@ plot!(Array(range(1,stop = datasize)),preds[11])
 plot!(Array(range(1,stop = datasize)),preds[12])
 plot!(Array(range(1,stop = datasize)),preds[13])
 # train n_ode with collocation method
-@time Flux.train!(L2_loss_fct, ps, data1, opt1, cb = cb1)
+@time Flux.train!(loss_fct, ps, data1, opt1, cb = cb1)
 
 derivs = []
 for i in test_u0s
@@ -104,5 +120,5 @@ plot!([1,2],[test_u0s[11], a[11]],label ="", color ="blue")
 plot!([1,2],[test_u0s[12], a[12]],label ="", color ="blue")
 plot!([1,2],[test_u0s[13], a[13]],label ="", color ="blue")
 hline!([-sqrt(alpha),0,sqrt(alpha)], label ="",color ="red")
-#savefig("alpha_5.pdf")
-#@save "pitchfork_bifur_alpha_5.bson" dudt
+savefig("alpha_ks_5.pdf")
+@save "pitchfork_bifur_alpha_ks_5.bson" dudt
