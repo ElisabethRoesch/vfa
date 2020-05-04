708a1798db65a8b41c92f20332aa2396073eb7c5
LislPisl <eroesch@student.unimelb.edu.au>
2020-05-04 10:13:52 +1000

    unsaved changes weekend

--- src_test/learning_bifurcations_latent_space/construct_latent_variables_for_bifurcation_params.jl
+++ src_test/learning_bifurcations_latent_space/construct_latent_variables_for_bifurcation_params.jl
@@ -21,7 +21,9 @@ species1 = "Pitchfork Bifurcation"
 alphas = [-1.5,-.8,-.5,-.2,0.,.2,.5,.8,1.5]
 inits = [-1.0, -0.6, -0.4, -0.1, 0.0, 0.1, 0.4, 0.6, 1.0]
 ind_alphas = [4,5,6]
-ind_inits = [1,2,3,4,5,6,7,8,9]
+#ind_inits = [1,2,3,4,5,6,7,8,9]
+ind_inits = [4,5,6]
+
 l_a, l_i = length(ind_alphas), length(ind_inits)
 
 # Fill array xes which contains all training data
@@ -30,44 +32,50 @@ counter_a = [1]
 counter_i = [1]
 for temp_ind_alpha in ind_alphas
     for temp_ind_init in ind_inits
-        print(temp_ind_alpha, temp_ind_init)
+        print(counter_a[1], counter_i[1])
         temp_x = read_bifurfile(bif_type, temp_ind_alpha, temp_ind_init)
         l_temp_x = length(temp_x)
         latent_space = Array{Float64,1}(undef,l_temp_x)
         latent_space[:] = Float64.(Array(range(1,l_temp_x)))
         temp_x_latent_space = vcat(temp_x, latent_space')
         xes[counter_a[1], counter_i[1]] = temp_x_latent_space
-        counter_i[1]=+1
+        counter_i[1]+=1
     end
-    counter_a[1]=+1
+    counter_i[1]=1
+    counter_a[1]+=1
 end
 
 # plot all xes
-counter_a2 = [1]
-counter_i2 = [1]
+counter_a = [1]
+counter_i = [1]
 plt_time = plot(grid = "off", xlab = "time", ylab = "Species U")
 for temp_ind_alpha in ind_alphas
     for temp_ind_init in ind_inits
-        scatter!(t, xes[counter_a2[1], counter_i2[1]][1,:], label = "")
-        counter_i2=+1
+        scatter!(t, xes[counter_a[1], counter_i[1]][1,:], label = "")
+        counter_i[1]+=1
     end
-    counter_a2=+1
+    counter_i[1]=1
+    counter_a[1]+=1
 end
 display(plt_time)
 
 # plot xes for same init in 2d
+counter_a = [1]
 plt_one_init_2d = plot(grid = "off", xlab = "time", ylab = "Species U", title = "One init, multiple alpha")
 for temp_ind_alpha in ind_alphas
     temp_ind_init = 3
-    scatter!(t, xes[temp_ind_alpha, temp_ind_init][1,:], label = "")
+    scatter!(t, xes[counter_a[1], temp_ind_init][1,:], label = "")
+    counter_a[1]+=1
 end
 display(plt_one_init_2d)
 
 # plot xes for same init in 3d
+counter_a = [1]
 plt_one_init_3d = plot(grid = "off", xlab = "time", ylab = "Species U")
 for temp_ind_alpha in ind_alphas
     temp_ind_init = 3
-    plot!(t, xes[temp_ind_alpha, temp_ind_init][1,:], xes[temp_ind_alpha, temp_ind_init][2,:], label = "")
+    plot!(t, xes[counter_a[1], temp_ind_init][1,:], xes[counter_a[1], temp_ind_init][2,:], label = "")
+    counter_a[1]+=1
 end
 display(plt_one_init_3d)
 
