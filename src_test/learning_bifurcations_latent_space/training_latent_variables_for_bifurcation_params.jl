9fdee12184b666413ce35263e965e1e79820ca2d
LislPisl <eroesch@student.unimelb.edu.au>
2020-05-08 10:36:46 +1000

    create file for training models for new training data

---
+++ src_test/learning_bifurcations_latent_space/training_latent_variables_for_bifurcation_params.jl
@@ -0,0 +1,4 @@
+push!(LOAD_PATH, "/Users/eroesch/github")
+using vfa, Plots, Optim, Dates, DiffEqParamEstim, Flux, DiffEqFlux, Statistics, LinearAlgebra, OrdinaryDiffEq
+using BSON: @save
+using DataFrames
