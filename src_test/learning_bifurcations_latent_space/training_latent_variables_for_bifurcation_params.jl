a532afca962d481f1834288a4d030a76f5f28150
LislPisl <eroesch@student.unimelb.edu.au>
2020-05-09 13:01:12 +1000

    create model

--- src_test/learning_bifurcations_latent_space/training_latent_variables_for_bifurcation_params.jl
+++ src_test/learning_bifurcations_latent_space/training_latent_variables_for_bifurcation_params.jl
@@ -2,3 +2,8 @@ push!(LOAD_PATH, "/Users/eroesch/github")
 using vfa, Plots, Optim, Dates, DiffEqParamEstim, Flux, DiffEqFlux, Statistics, LinearAlgebra, OrdinaryDiffEq
 using BSON: @save
 using DataFrames
+
+
+dudt = Chain(Dense(1,15,tanh),
+       Dense(15,15,tanh),
+       Dense(15,1))
